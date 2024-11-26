c34b60286a3f1f4c4e38fd277827b11aea0a935e
lucif0124 <lucif040124@gmail.com>
2024-11-26 17:29:59 +0900

    Add files via upload

--- Main.js
+++ Main.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const FileStore = require('session-file-store')(session)
 
 var authRouter = require('./lib_login/auth');
-var authCheck = require('./lib_login/authcheck.js');
+var authCheck = require('./lib_login/authCheck.js');
 
 const app = express()
 const port = 3000
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
   if (!authCheck.isOwner(req, res)) {  // 로그인 안되어있으면 로그인 페이지로 이동시킴
     res.redirect('/auth/login');
     return false;
-  } else {                                      // 로그인 되어있으면 메인 페이지로 이동시킴
+  } else {                             // 로그인 되어있으면 메인 페이지로 이동시킴
     res.redirect('/main');
     return false;
   }
@@ -38,13 +38,7 @@ app.get('/main', (req, res) => {
     res.redirect('/auth/login');
     return false;
   }
-  var html = template.HTML('Welcome',
-    `<hr>
-        <h2>메인 페이지에 오신 것을 환영합니다</h2>
-        <p>로그인에 성공하셨습니다.</p>`,
-    authCheck.statusUI(req, res)
-  );
-  res.send(html);
+  res.redirect('/mainpage.html');
 })
 
 app.listen(port, () => {
